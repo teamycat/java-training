3b1b4a1323a2c6af2798bde2fe167ba0dabb1bdf
Edwin <edwinzzhao@gmail.com>
2022-03-09 22:02:03 -0500

    learn variable

--- src/com/ghyll/training/StudyTime.java
+++ src/com/ghyll/training/StudyTime.java
@@ -3,7 +3,13 @@ package com.ghyll.training;
 class StudyTime {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // System.out.println("Hello World to: " + args[0] + " and " +  args[1]);
+        String name = "Hello New World 2"; 
+        System.out.println(name);
+        //System.out.println("Hello New World");
+
+
+        return;
     }
 
 }
